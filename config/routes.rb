94689063d57f918b4a204f0bf35758311f4261c9
KRofLife <05410kira@gmail.com>
2021-12-23 01:56:13 +0000

    ItemのCRUD機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'addresses/index'
+  get 'addresses/edit'
+  get 'addresses/show'
+  resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  # get 'items/index'
+  # get 'items/edit'
+  # get 'items/show'
+  get 'categories/index'
+  get 'categories/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
 end
